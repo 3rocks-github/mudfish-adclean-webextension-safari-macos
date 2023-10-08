eafbf6d7c838f15def182a23e89aae8097e272e5
Weongyo Jeong <weongyo@3rocks.net>
2023-10-07 20:56:33 -0700

    .

---
+++ Mudfish Adclean/Mudfish Adclean/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Mudfish Adclean
+//
+//  Created by Weongyo Jeong on 10/7/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
